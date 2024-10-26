def90d0dfd126fb4b3b1ae870f4927aabd06afa8
Bondarev Maxim <bonmax46@mail.ru>
2024-10-26 19:47:53 +0300

    initial commit

---
+++ dz_interview.py
@@ -0,0 +1,55 @@
+
+
+class Stack:
+    def __init__(self):
+        self.list_stack = []
+
+    def is_empty(self):
+        return True if len(self.list_stack) == 0 else False
+
+    def push(self, item):
+        self.list_stack.append(item)
+
+    def pop_(self):
+        return self.list_stack.pop()
+
+    def peek(self):
+        return self.list_stack[-1]
+
+    def size_(self):
+        return len(self.list_stack)
+
+
+def stack_balance(sequence):
+    STAPLES_DICT = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    stack_balance = Stack()
+    balanced = True
+    index = 0
+    while index < len(sequence) and balanced:
+        symbol = sequence[index]
+        if symbol in STAPLES_DICT:
+            stack_balance.push(symbol)
+        else:
+            if stack_balance.is_empty():
+                balanced = False
+            else:
+                if symbol == STAPLES_DICT.get(stack_balance.peek()):
+                    stack_balance.pop_()
+                else:
+                    balanced = False
+        index = index + 1
+    if balanced and stack_balance.is_empty():
+        return True
+    else:
+        return False
+
+
+print(stack_balance("(([)])"))
+
+
+
+
